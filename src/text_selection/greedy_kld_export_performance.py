322c026cda8e8f6ee6a8ce52274f9848f00fbfe0
Stefan Taubert <stefan.taubert@posteo.de>
2021-11-12 15:59:28 +0100

    .

--- src/text_selection/greedy_kld_export_performance.py
+++ src/text_selection/greedy_kld_export_performance.py
@@ -9,6 +9,7 @@ from typing import OrderedDict as OrderedDictType
 from typing import Set, Tuple, Union
 
 import numpy as np
+import scipy as sp
 from ordered_set import OrderedSet
 from scipy.stats import entropy
 from tqdm import tqdm
@@ -19,30 +20,109 @@ from text_selection.greedy_kld_methods import (__get_distribution,
                                                get_uniform_distribution,
                                                sync_dict_keys_to_keys_inplace)
 from text_selection.selection import SelectionMode, order_keys, select_key
-from text_selection.utils import (DurationBoundary,
+from text_selection.utils import (DurationBoundary, filter_after_duration,
                                   filter_data_durations_number_inplace,
                                   get_distribution, get_ngrams)
 
 NGram = int
-NGrams = Tuple[NGram, ...]
+NGrams = np.ndarray
 
 
-def greedy_kld_uniform_ngrams_seconds_with_preselection_perf(data: OrderedDictType[int, List[str]], n_gram: int, ignore_symbols: Optional[Set[str]], durations_s: Dict[int, float], seconds: float, preselection: OrderedDictType[int, List[str]], duration_boundary: DurationBoundary, n_jobs: int, maxtasksperchild: Optional[int], chunksize: Optional[int]) -> OrderedSet[int]:
-  data_ngrams = get_filtered_ngrams_numbers(data, n_gram, ignore_symbols)
-  filter_data_durations_number_inplace(data_ngrams, durations_s, duration_boundary)
-  preselection_ngrams = get_filtered_ngrams_numbers(preselection, n_gram, ignore_symbols)
+def get_uniform_distribution_ngrams(ngrams: Dict[Tuple[str, ...], NGrams]) -> Dict[NGram, float]:
+  if len(ngrams) == 0:
+    return dict()
+  distr = 1 / len(ngrams)
+  res: Dict[NGram, float] = {ngram_nr: distr for _, ngram_nr in ngrams.items()}
+  return res
+
+
+def get_chunksize(data_count: int, n_jobs: int, chunksize: Optional[int], batches: Optional[int]) -> int:
+  if batches is None:
+    assert chunksize is not None
+    assert chunksize > 0
+    return chunksize
+
+  if data_count == 0:
+    return 1
+  chunksize = math.ceil(data_count / n_jobs / batches)
+  return chunksize
+
+
+def greedy_kld_uniform_ngrams_seconds_with_preselection_perf(data: Dict[int, Tuple[str, ...]], select_from_keys: OrderedSet[int], preselection_keys: Set[int], n_gram: int, ignore_symbols: Optional[Set[str]], select_from_durations_s: Dict[int, float], seconds: float, duration_boundary: DurationBoundary, n_jobs: int, maxtasksperchild: int, chunksize: Optional[int], batches: Optional[int]) -> OrderedSet[int]:
+  logger = getLogger(__name__)
+
+  select_from_keys = get_duration_keys(select_from_durations_s, select_from_keys, duration_boundary)
+
+  logger.info(f"Collecting data symbols...")
+  data_symbols = get_unique_symbols(data, select_from_keys)
+  logger.info(f"Collecting preselection symbols...")
+  preselection_symbols = get_unique_symbols(data, preselection_keys)
+  target_symbols = data_symbols | preselection_symbols
+  if ignore_symbols is None:
+    target_symbols -= ignore_symbols
+
+  logger.info(f"Calculating {n_gram}-grams...")
+  data_ngrams, ngram_nr_to_ngram = get_ngrams_from_data(
+    data=data,
+    keys=select_from_keys,
+    n_gram=n_gram,
+    target_symbols=target_symbols,
+    n_jobs=n_jobs,
+    maxtasksperchild=maxtasksperchild,
+    chunksize=chunksize,
+    batches=batches,
+  )
+
+  # keymap = {
+  #   i: data_key
+  #   for i, data_key in enumerate(duration_keys)
+  # }
+
+  # tmp_array = np.array(list(data_ngrams.values()))
+
+  #filter_data_durations_number_inplace(data_ngrams, durations_s, duration_boundary)
+  preselection_ngrams, ngram_nr_to_ngram_preselection = get_ngrams_from_data(
+    data=data,
+    keys=preselection_keys,
+    n_gram=n_gram,
+    target_symbols=target_symbols,
+    n_jobs=n_jobs,
+    maxtasksperchild=maxtasksperchild,
+    chunksize=chunksize,
+    batches=batches,
+  )
+
+  assert ngram_nr_to_ngram == ngram_nr_to_ngram_preselection
+  all_n_grams = ngram_nr_to_ngram
+
+  # if ignore_symbols is not None and len(ignore_symbols) > 0:
+  #   ignore_ngrams_inplace(data_ngrams, ignore_symbols, ngram_nr_to_ngram)
+  #   ignore_ngrams_inplace(preselection_ngrams, ignore_symbols, preselection_ngram_nr_to_ngram)
 
   logger = getLogger(__name__)
-  uniform_distr = get_uniform_distribution(data_ngrams)
+  uniform_distr = get_uniform_distribution_ngrams(all_n_grams)
   if len(uniform_distr) > 0:
     logger.info(f"Target uniform distribution: {list(uniform_distr.values())[0]}")
+
   if chunksize is None:
     chunksize = get_chunksize_for_data(data_ngrams, n_jobs)
 
+  np_counts = get_counts_array(
+    data_ngrams,
+    select_from_keys,
+    all_n_grams,
+    n_jobs=n_jobs,
+    chunksize=chunksize,
+    maxtasksperchild=maxtasksperchild,
+    batches=batches,
+  )
+
   greedy_selected = sort_greedy_kld_until_with_preselection(
     data=data_ngrams,
+    np_counts=np_counts,
+    data_key_order=select_from_keys,
     target_dist=uniform_distr,
-    until_values=durations_s,
+    until_values=select_from_durations_s,
     until_value=seconds,
     preselection=preselection_ngrams,
     n_jobs=n_jobs,
@@ -51,6 +131,96 @@ def greedy_kld_uniform_ngrams_seconds_with_preselection_perf(data: OrderedDictTy
   )
 
   return greedy_selected
+
+
+process_data_ngrams: Dict[int, NGram] = None
+process_ngram_nrs: OrderedSet[NGram] = None
+
+
+def init_pool_np_counts(data_ngrams: Dict[int, NGram], ngram_nrs: OrderedSet[NGram]) -> None:
+  global process_data_ngrams
+  global process_ngram_nrs
+  process_data_ngrams = data_ngrams
+  process_ngram_nrs = ngram_nrs
+
+
+def main_np_counts(i_k: Tuple[int, int]) -> Tuple[int, np.ndarray]:
+  global process_data_ngrams
+  global process_ngram_nrs
+  i, k = i_k
+
+  ngrams = process_data_ngrams[k]
+  counts = get_available_array(ngrams, process_ngram_nrs)
+  return i, counts
+
+
+def log_mp_params(n_jobs: int, chunksize: int, maxtasksperchild: int, data_count: int) -> None:
+  logger = getLogger(__name__)
+  logger.info(
+    f"Using {n_jobs} processes with chunks of size {chunksize} for {data_count} utterances and maxtask per child: {maxtasksperchild}.")
+
+
+def get_counts_array(data_ngrams: Dict[int, NGram], select_from_keys: Set[int], all_n_grams: Dict[Tuple[str, ...], NGram], n_jobs: int, chunksize: Optional[int], maxtasksperchild: int, batches: Optional[int]):
+  logger = getLogger(__name__)
+  logger.info("Calculating counts array..")
+  ngram_nrs = OrderedSet(all_n_grams.values())
+  np_counts = np.zeros(shape=(len(select_from_keys), len(ngram_nrs)), dtype=np.uint32)
+
+  # for i, k in enumerate(tqdm(select_from_keys)):
+  #   ngrams = data_ngrams[k]
+  #   counts = get_available_array(ngrams, ngram_nrs)
+  #   np_counts[i] = counts
+
+  chunksize = get_chunksize(len(select_from_keys), n_jobs, chunksize, batches)
+  log_mp_params(n_jobs, chunksize, maxtasksperchild, len(select_from_keys))
+
+  with Pool(
+    processes=n_jobs,
+    initializer=init_pool_np_counts,
+    initargs=(data_ngrams, ngram_nrs),
+    maxtasksperchild=maxtasksperchild,
+  ) as pool:
+    with tqdm(total=len(select_from_keys)) as pbar:
+      iterator = pool.imap_unordered(
+        main_np_counts, enumerate(select_from_keys), chunksize=chunksize)
+      for index, counts in iterator:
+        np_counts[index] = counts
+        pbar.update()
+
+  return np_counts
+
+
+def get_duration_keys(durations: Dict[int, float], keys: Set[int], boundary: DurationBoundary) -> OrderedSet[int]:
+  logger = getLogger(__name__)
+  boundary_min, boundary_max = boundary
+  logger.info("Getting entries maching duratino boundary...")
+  filtered_utterance_ids = get_keys_in_duration_boundary(
+    durations, keys, boundary_min, boundary_max)
+  not_selected_utterances_out_of_boundary = len(keys) - len(filtered_utterance_ids)
+
+  if not_selected_utterances_out_of_boundary > 0:
+    logger.warning(
+        f"Missed out utterances due to duration boundary [{boundary_min},{boundary_max}): {not_selected_utterances_out_of_boundary}/{len(keys)} ({not_selected_utterances_out_of_boundary/len(keys)*100:.2f}%) -> retrieved {len(filtered_utterance_ids)} entries.")
+  else:
+    logger.debug(
+      f"Didn't missed out any utterances through boundary [{boundary_min},{boundary_max}) -> kept {len(filtered_utterance_ids)} entries.")
+
+  return filtered_utterance_ids
+
+
+def get_keys_in_duration_boundary(corpus: Dict[int, float], keys: OrderedSet[int], min_duration_incl: float, max_duration_excl: float) -> OrderedSet[int]:
+  assert min_duration_incl >= 0
+  assert max_duration_excl >= 0
+
+  filtered_utterance_indicies: OrderedSet[int] = OrderedSet()
+
+  for utterance_id in tqdm(keys):
+    assert utterance_id in corpus
+    utterance_duration = corpus[utterance_id]
+    if min_duration_incl <= utterance_duration < max_duration_excl:
+      filtered_utterance_indicies.add(utterance_id)
+
+  return filtered_utterance_indicies
 
 
 process_ngram_nr_to_ngram: Dict[Tuple[str, ...], NGram] = None
@@ -63,7 +233,14 @@ def main(key: int, n: int) -> Tuple[int, NGrams]:
   n_gram = process_data[key]
 
   ngrams = get_ngrams(n_gram, n)
-  ngrams_int = tuple(process_ngram_nr_to_ngram[ngram] for ngram in ngrams)
+  ngram_nrs = tuple(
+    process_ngram_nr_to_ngram[ngram]
+    for ngram in ngrams
+    if ngram in process_ngram_nr_to_ngram
+  )
+
+  ngrams_int = np.array(ngram_nrs, dtype=np.uint32)
+
   return key, ngrams_int
 
 
@@ -74,28 +251,30 @@ def init_pool_ngrams(data: OrderedDictType[int, Tuple[str, ...]], ngram_nr_to_ng
   process_ngram_nr_to_ngram = ngram_nr_to_ngram
 
 
-def get_filtered_ngrams_numbers(data: OrderedDictType[int, Tuple[str, ...]], n_gram: int, ignore_symbols: Optional[Set[str]]) -> OrderedDictType[int, NGrams]:
-  assert isinstance(data, OrderedDict)
+def get_unique_symbols(data: Dict[int, Tuple[str, ...]], keys: Set[int]) -> Set[str]:
+  occurring_symbols = {symbol for key in tqdm(keys, total=len(keys)) for symbol in data[key]}
+  return occurring_symbols
 
+
+def get_ngrams_from_data(data: OrderedDictType[int, Tuple[str, ...]], keys: Set[int], target_symbols: Set[str], n_gram: int, n_jobs: int, maxtasksperchild: int, chunksize: Optional[int], batches: Optional[int]) -> Tuple[Dict[int, NGrams], Dict[Tuple[str, ...], NGrams]]:
   logger = getLogger(__name__)
-  logger.info(f"Calculating {n_gram}-grams...")
-
-  #ngram_nr_to_ngram: Dict[Tuple[str, ...], NGram] = {}
-
-  logger.info(f"Collecting all symbols...")
-  occurring_symbols = {x for y in tqdm(data.values(), total=len(data)) for x in y}
 
   logger.info(f"Calculating all possible {n_gram}-grams...")
   possible_ngrams = list(tqdm(itertools.permutations(
-    occurring_symbols, r=n_gram), total=len(occurring_symbols) ** n_gram))
+    target_symbols, r=n_gram), total=len(target_symbols) ** n_gram))
 
-  ngram_nr_to_ngram: OrderedDictType[int, NGrams] = OrderedDict([
-    (k, i) for i, k in enumerate(possible_ngrams)
-  ])
+  ngram_nr_to_ngram: Dict[Tuple[str, ...], NGrams] = {
+    k: i for i, k in enumerate(possible_ngrams)
+  }
 
-  n_jobs = cpu_count()
-  # todo smaller chunksize
-  chunksize = get_chunksize_for_data(data, n_jobs)
+  if len(keys) == 0 or (chunksize is not None and chunksize <= 0):
+    chunksize = 1
+  if batches is not None:
+    chunksize = math.ceil(len(keys) / n_jobs / batches)
+
+  if chunksize == 0:
+    chunksize = 1
+
   method_proxy = partial(
     main,
     n=n_gram,
@@ -105,63 +284,15 @@ def get_filtered_ngrams_numbers(data: OrderedDictType[int, Tuple[str, ...]], n_g
       processes=n_jobs,
       initializer=init_pool_ngrams,
       initargs=(data, ngram_nr_to_ngram),
-      maxtasksperchild=None,
+      maxtasksperchild=maxtasksperchild,
     ) as pool:
     available_ngrams: Dict[int, NGrams] = OrderedDict(tqdm(
-      pool.imap_unordered(method_proxy, data.keys(), chunksize=chunksize),
-      total=len(data),
+      pool.imap_unordered(method_proxy, keys, chunksize=chunksize),
+      total=len(keys),
     ))
   logger.info("Done.")
 
-  # for k, v in tqdm(data.items(), total=len(data)):
-  #   ngrams = get_ngrams(v, n_gram)
-  #   ngrams_int = tuple(possible_ngrams[ngram] for ngram in ngrams)
-  #   # for ngram in ngrams:
-  #   #   if ngram in ngram_nr_to_ngram:
-  #   #     ngrams_int.append(ngram_nr_to_ngram[ngram])
-  #   #   else:
-  #   #     new_nr = len(ngram_nr_to_ngram)
-  #   #     ngram_nr_to_ngram[ngram] = new_nr
-  #   #     ngrams_int.append(new_nr)
-  #   available_ngrams[k] = ngrams_int
-
-  occurring_symbols_count = len(occurring_symbols)
-  occurring_ngrams_count = len(ngram_nr_to_ngram)
-
-  logger.info(
-      f"Theoretically, the maximum amount of unique {n_gram}-grams is: {occurring_symbols_count ** n_gram}.")
-
-  if occurring_symbols_count > 0:
-    logger.info(
-      f"The amount of unique occurring {n_gram}-grams is: {occurring_ngrams_count} ({occurring_ngrams_count/(occurring_symbols_count ** n_gram)*100:.2f}%).")
-
-  if ignore_symbols is not None:
-    occuring_ignore_symbols = occurring_symbols.intersection(ignore_symbols)
-
-    if len(occuring_ignore_symbols) > 0:
-      logger.info(
-        f"Removing {n_gram}-grams which contain: \"{', '.join(list(sorted(occuring_ignore_symbols)))}\"...")
-
-      ignore_ngram_nrs = {
-        ngram_nr for ngram, ngram_nr in ngram_nr_to_ngram.items()
-        if len(set(ngram).intersection(ignore_symbols)) > 0
-      }
-
-      for k, ngram_nrs in available_ngrams.items():
-        ngram_nrs_filtered = tuple(
-          ngram_nr for ngram_nr in ngram_nrs
-          if ngram_nr not in ignore_ngram_nrs
-        )
-        changed_something = len(ngram_nrs_filtered) != len(ngram_nrs)
-        if changed_something:
-          available_ngrams[k] = ngram_nrs_filtered
-
-      new_occurring_ngrams_count = len(ngram_nr_to_ngram) - len(ignore_ngram_nrs)
-
-      logger.info(
-          f"Removed {occurring_ngrams_count - new_occurring_ngrams_count} unique {n_gram}-gram(s).")
-
-  return available_ngrams
+  return available_ngrams, ngram_nr_to_ngram
 
 
 def get_available_array(ngrams: NGrams, target_symbols_ordered: OrderedSet[NGram]) -> np.ndarray:
@@ -170,7 +301,7 @@ def get_available_array(ngrams: NGrams, target_symbols_ordered: OrderedSet[NGram
   return dict_to_array_ordered(counts, target_symbols_ordered)
 
 
-def sort_greedy_kld_until_with_preselection(data: OrderedDictType[int, NGrams], target_dist: Dict[NGram, float], until_values: Dict[int, Union[float, int]], until_value: Union[float, int], preselection: OrderedDictType[int, NGrams], n_jobs: int, maxtasksperchild: Optional[int], chunksize: int) -> OrderedSet[int]:
+def sort_greedy_kld_until_with_preselection(data: OrderedDictType[int, NGrams], np_counts: np.ndarray, data_key_order: OrderedSet[int], target_dist: Dict[NGram, float], until_values: Dict[int, Union[float, int]], until_value: Union[float, int], preselection: OrderedDictType[int, NGrams], n_jobs: int, maxtasksperchild: Optional[int], chunksize: int) -> OrderedSet[int]:
   assert isinstance(data, OrderedDict)
   assert isinstance(preselection, OrderedDict)
   # The probability is really high that only one key is figured out, therefore it is useless to use any selection modes. If shortest or longest should be used the unfiltered count of symbols needs to be passed as extra parameter which increases complexity of the method.
@@ -179,7 +310,7 @@ def sort_greedy_kld_until_with_preselection(data: OrderedDictType[int, NGrams], 
   result: OrderedSet[int] = OrderedSet()
   target_symbols_ordered: OrderedSet[NGram] = OrderedSet(sorted(target_dist.keys()))
   # defines the order for what the selection is based on
-  available_data_keys_ordered = OrderedSet(data.keys())
+  available_data_keys_ordered = data_key_order.copy()
   # all_occuring_values: Set[_T2] = {x for y in data.values() for x in y}
   # assert all_keys == all_occuring_values
 
@@ -297,6 +428,7 @@ def get_divergence_for_utterance(key: int) -> Tuple[int, float]:
   global process_covered_counts
   global process_target_dist
   global process_target_symbols_ordered
+  assert key in process_data
   utterance = process_data[key]
   utterance_counts = get_available_array(utterance, process_target_symbols_ordered)
   counts = process_covered_counts + utterance_counts
--- src/text_selection_tests/test_greedy_kld_export_performance.py
+++ src/text_selection_tests/test_greedy_kld_export_performance.py
@@ -6,7 +6,6 @@ from text_selection.greedy_kld_export_performance import \
 
 
 def test_sort_greedy_kld_until_with_preselection__without_preselection():
-  preselection = OrderedDict()
   data = OrderedDict([
     (5, ("a", "b")),
     (6, ("b",)),
@@ -23,15 +22,17 @@ def test_sort_greedy_kld_until_with_preselection__without_preselection():
 
   res = greedy_kld_uniform_ngrams_seconds_with_preselection_perf(
     data=data,
-    durations_s=durations,
     seconds=target_duration,
-    preselection=preselection,
+    preselection_keys=OrderedSet(),
+    select_from_durations_s=durations,
+    select_from_keys=data.keys(),
     ignore_symbols={"b"},
     n_gram=1,
-    chunksize=1,
     duration_boundary=(0, 2),
     maxtasksperchild=None,
     n_jobs=1,
+    batches=None,
+    chunksize=1,
   )
 
   assert OrderedSet([5, 7]) == res

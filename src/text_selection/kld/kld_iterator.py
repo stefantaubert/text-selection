1ee2637d37c2a0d4ed14e5a90498c116e5ca9f49
Stefan Taubert <stefan.taubert@posteo.de>
2022-05-20 16:03:15 +0200

    fix typo

--- src/text_selection/kld/kld_iterator.py
+++ src/text_selection/kld/kld_iterator.py
@@ -102,12 +102,12 @@ def get_minimum_kld(counts: np.ndarray, target_distribution: np.ndarray) -> Tupl
     new_counts_distributions, target_distribution, axis=1)
   del new_counts_distributions
   assert np.all(divergences >= 0.0)
-  min_divergence, min_indices = get_minimun_indices(divergences)
+  min_divergence, min_indices = get_minimum_indices(divergences)
   del divergences
   return min_divergence, min_indices
 
 
-def get_minimun_indices(array: np.ndarray) -> Tuple[float, np.ndarray]:
+def get_minimum_indices(array: np.ndarray) -> Tuple[float, np.ndarray]:
   assert len(array) > 0
   min_value = array.min()
   min_indices = np.flatnonzero(array == min_value)

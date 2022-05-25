0d4e4c46a0ca09cdd4a7e49fdcb0846769637e8e
Stefan Taubert <stefan.taubert@posteo.de>
2022-05-25 17:16:14 +0200

    removed api

--- src/text_selection_api/__init__.py
+++
@@ -1,4 +0,0 @@
-from text_selection_core.types import (LineNr, LineNrs, Dataset, Lines,
-                                       DataWeights, Subset, SubsetName)
-
-from text_selection_api.api import create, get_selection

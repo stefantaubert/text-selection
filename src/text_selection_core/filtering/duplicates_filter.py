4238b8f68db5df914d2bb85a461720407803f951
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2022-11-07 11:42:12 +0100

    improvements

--- src/text_selection_core/filtering/duplicates_filter.py
+++ src/text_selection_core/filtering/duplicates_filter.py
@@ -1,6 +1,6 @@
 import hashlib
 from logging import Logger, getLogger
-from typing import Optional
+from typing import Iterable, Optional
 
 from ordered_set import OrderedSet
 from tqdm import tqdm
@@ -9,7 +9,7 @@ from text_selection_core.common import (SelectionDefaultParameters,
                                         validate_selection_default_parameters)
 from text_selection_core.globals import TQDM_LINE_UNIT, ExecutionResult
 from text_selection_core.helper import get_percent_str
-from text_selection_core.types import (Lines, Subset, get_subsets_line_nrs_count,
+from text_selection_core.types import (LineNr, Lines, Subset, get_subsets_line_nrs_count,
                                        get_subsets_line_nrs_gen, move_lines_to_subset)
 from text_selection_core.validation import ensure_lines_count_matches_dataset
 
@@ -29,23 +29,9 @@ def filter_duplicates(default_params: SelectionDefaultParameters, lines: Lines, 
   select_from_count = get_subsets_line_nrs_count(
     default_params.dataset, default_params. from_subset_names)
 
-  result: Subset = OrderedSet()
-  collected = set()
-  line_nrs = tqdm(select_from_line_nrs, desc="Filtering duplicates",
-                  unit=TQDM_LINE_UNIT, total=select_from_count)
-  for line_nr in line_nrs:
-    # TODO think of using hash of line instead of line
-    line = lines[line_nr]
-    line_hash = hashlib.sha256(line.encode(encoding)).hexdigest()
-    if line_hash in collected:
-      result.add(line_nr)
-      logger.info(f"Filtered L-{line_nr+1}: \"{lines[line_nr]}\".")
-    else:
-      collected.add(line_hash)
-    del line
-    del line_hash
-    del line_nr
-  del collected
+  with tqdm(select_from_line_nrs, desc="Filtering duplicates",
+            unit=TQDM_LINE_UNIT, total=select_from_count) as line_nrs:
+    result: Subset = filter_without_hash(line_nrs, lines, logger)
 
   if changed_anything := len(result) > 0:
     logger.info(
@@ -55,3 +41,38 @@ def filter_duplicates(default_params: SelectionDefaultParameters, lines: Lines, 
     logger.info("No duplicate lines exist!")
 
   return changed_anything
+
+
+# is inefficient: @100mio lines ~20 GB ram usage
+# def filter_with_hash(line_nrs: Iterable[LineNr], encoding: str, lines: Lines, logger: Logger) -> OrderedSet:
+#   result: Subset = OrderedSet()
+#   collected = set()
+#   for line_nr in line_nrs:
+#     line = lines[line_nr]
+#     line_hash = hashlib.sha256(line.encode(encoding)).hexdigest()
+#     if line_hash in collected:
+#       result.add(line_nr)
+#       logger.info(f"Filtered L-{line_nr+1}: \"{line}\".")
+#     else:
+#       collected.add(line_hash)
+#     del line
+#     del line_hash
+#     del line_nr
+#   del collected
+
+
+def filter_without_hash(line_nrs: Iterable[LineNr], lines: Lines, logger: Logger) -> Subset:
+  # @100mio lines ~5 GB ram usage
+  result: Subset = OrderedSet()
+  collected = set()
+  for line_nr in line_nrs:
+    line = lines[line_nr]
+    if line in collected:
+      result.add(line_nr)
+      logger.info(f"Filtered L-{line_nr+1}: \"{line}\".")
+    else:
+      collected.add(line)
+    del line
+    del line_nr
+  del collected
+  return result
